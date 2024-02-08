4ae27eecd75a18b4ce83567ce5c6669ef12366e4
cadamar1236 <73383629+cadamar1236@users.noreply.github.com>
2024-02-08 20:48:47 +0100

    nuevo subida

--- mi-proyecto-web/app/__init__.py
+++ mi-proyecto-web/app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from datetime import datetime
+# Inicializa la aplicación Flask
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'C:\\Users\\User\\Desktop\\Proyecto\\Proyecto'
+# Establece una clave secreta para tu aplicación
+app.config['SECRET_KEY'] = 'edtech1245'
+@app.context_processor
+def inject_now():
+    return {'now': datetime.now()}
+from app import views
 
-def create_app():
-    app = Flask(__name__)
-    from .views import main
-    app.register_blueprint(main)
-    return app
 
 
--- mi-proyecto-web/app/views.py
+++ mi-proyecto-web/app/views.py
@@ -1,15 +1,15 @@
 # views.py
-from flask import render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from .forms import AudioUploadForm, YouTubeForm
 from .util import whisper_util
 import os
+from app import app
+from datetime import datetime
 
-# Importamos la instancia de la aplicación
-app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('base.html', now=datetime.now())
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_audio():
@@ -21,13 +21,21 @@ def upload_audio():
         return render_template('transcription.html', transcription=transcription)
     return render_template('upload.html', form=form)
 
+
+
 @app.route('/transcribe_youtube', methods=['GET', 'POST'])
 def transcribe_youtube():
     form = YouTubeForm()
     if form.validate_on_submit():
-        youtube_url = form.youtube_url.data
-        transcription = whisper_util.process_youtube_video(youtube_url)
-        return render_template('transcription.html', transcription=transcription)
+        try:
+            youtube_url = form.youtube_url.data
+            transcription = whisper_util.process_youtube_video(youtube_url)
+            return render_template('transcription.html', transcription=transcription)
+        except FileNotFoundError:
+            Flasklash('Error al procesar el video. Asegúrate de que el enlace sea correcto.', 'error')
+        except Exception as e:
+            Flash('Ocurrió un error inesperado. Por favor, inténtalo de nuevo.', 'error')
+            # Log the exception e for debugging purposes
     return render_template('youtube_transcription.html', form=form)
 
 if __name__ == '__main__':
